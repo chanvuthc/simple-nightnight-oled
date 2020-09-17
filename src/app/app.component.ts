c92dd6161f5200531ee96592176a4c496b2a925e
Chea ChanVuth <chea.chanvuth@ababank.com>
2020-09-17 20:23:48 +0700

    Basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,32 @@
 import { Component } from '@angular/core';
-
-import { Platform } from '@ionic/angular';
+import { AndroidFullScreen } from '@ionic-native/android-full-screen/ngx';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private androidFullScreen: AndroidFullScreen
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
+      this.statusBar.hide();
       this.splashScreen.hide();
+    });
+
+    this.androidFullScreen.isImmersiveModeSupported().then(() => {
+      this.androidFullScreen.immersiveMode();
     });
   }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  // Settings
+  activeColor = '#ADD8E6';
+  switchDuration = 5;
 
-  constructor() {}
+  // Meta
+  isShowSettings = true;
+  isTopActive = true;
+  lastTimeout;
+  lastInterval;
 
+  constructor() {
+    this.updateInterval();
+  }
+
+  updateInterval() {
+    if (this.lastInterval) {
+      clearInterval(this.lastInterval);
+    }
+
+    this.lastInterval = setInterval(() => {
+      this.isTopActive = !this.isTopActive;
+    }, this.switchDuration * 1000);
+  }
+
+  showSettings() {
+    this.isShowSettings = true;
+
+    if (this.lastTimeout) {
+      clearTimeout(this.lastTimeout);
+    }
+
+    this.lastTimeout = setTimeout(() => {
+      this.isShowSettings = false;
+      this.lastTimeout = undefined;
+    }, 5000);
+  }
 }
